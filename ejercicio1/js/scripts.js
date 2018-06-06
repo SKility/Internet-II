e558b789a95dff06d63033f9f144d3077462c775
SKility <santiago.keilty@gmail.com>
2018-06-06 19:30:14 -0300

    6/6

--- ejercicio1/js/scripts.js
+++ ejercicio1/js/scripts.js
@@ -1,23 +1,31 @@
 var listusers={
     listall:function(){
-        for(var i=0;i<listusers.users.length;i++){
-            $("#table-body span").append(listusers.assamble(listusers.users[i]));
+        for(var i=0;i<listusers.items.length;i++){
+            $("#table-body").append(listusers.assamble(this.items[i]));
        };
-       
-    
     },
-    assamble:function(elements){
-        return "<strong>"
-         +"<strong>"+elements.brand+"</strong>"
-         +"<strong>"+elements.location+"</strong>"
-         +"<strong>"+elements.lunch+"</strong>"
-         +"<strong>"+elements.price+"</strong>"
-         +"</strong>";
+    assamble:function(items){
+        return '<div class="row">'+
+        '<span class="col-2">'+'</span>' +
+        '<span class="col-2">'+ items.brand + '</span>' +
+        '<span class="col-2">'+ items.location+'</span>' +
+        '<span class="col-2">'+ items.lunch+'</span>' +
+        '<span class="col-2">'+ items.price+'</span>' +
+        '<span class="col-2">'+'</span>' +
+        '<span class="description col-12">'+'</span>' +
+      '</div>'
     },
-    users:[],
-    setusers:function(elements){
-    listusers.users=elements
+    items:[],
+    setusers:function(items){
+    listusers.items=items
+    },
+    log:function(item){
+        console.log(this.items[item]);
     }
 }
 
-var items=["brand", "location", "lunch", "price"]
+function asd(){
+    listusers.setusers(elementsList);
+    listusers.listall();
+}
+
