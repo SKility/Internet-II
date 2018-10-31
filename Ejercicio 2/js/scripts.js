98961257962cbb76f7d206e61026c75940420007
SKility <38074752+SKility@users.noreply.github.com>
2018-10-31 13:19:12 -0300

    Add files via upload
    Trabajo Practico servidor de peliculas

--- Ejercicio 2/js/scripts.js
+++ Ejercicio 2/js/scripts.js
@@ -55,41 +55,14 @@ function getAllFilms() {
         }).then(exito, fallo)
 
         function exito(response) {
-                listElements.setItems(response);
-                listElements.listAll()
+                listId.setItems(response);
+                listId.listAll()
         }
         function fallo(err) {
                 console.log("Error wachin");
         }
+}      
 
-        var listElements = {
-                items: [],
-                setItems(data) {
-                        listElements.items = data;
-                },
-                listAll: function () {
-                        for (var i = 0; listElements.items.length > i; i++) {
-                                console.log("for");
-                                $(".traer").append(this.assamble(this.items[i]));
-                        };
-                },
-
-                assamble: function (items) {
-                        return '<div class="Item">'
-                                + '<h1>Peliculas</h1>'
-                                + '<p>' + items.title + '</p>'
-                                + '<p>' + items.description + '</p>'
-                                + '<p>' + items.cast + '</p>'
-                                + '<p>' + items.gender + '</p>'
-                                + '<p>' + items.poster + '</p>'
-                                + '<p>' + items.score + '</p>'
-                                + '</div>'
-                }
-        }
-
-
-
-}
 function getById(id) {
         return $.ajax({
                 url: "http://192.168.1.115:3000/peliculas/" + id,
@@ -118,49 +91,64 @@ var listId = {
                 listId.items = data;
         },
         listAll: function () {
-                for (var i = 0; listId.items.length > i; i++) {
-                        console.log("for");
-                        $(".traer").append(this.assamble(listId.items[i]));
+                for (var i = 0; this.items.length > i; i++) {
+                        $(".Contenedor").append(this.assamble(listId.items[i]));
                 }
-                $(".detallepeli").on("click", function () {
-                        var id = $(this).attr('data-id')
-                        //Esto aplica un atributo al objeto this($("detallepeli"))//
-                        console.log(id);
-                        getById(id).then(function (exito) {
-                                console.log(exito)
-                        $("traer").html(singleassamble(exito))
-                        })
+
+                $(".Pelicula").on("click", function () {
+                        var id = $(this).data('id')
+                        
+                        for(var i = 0; i < listId.items.length; i++){
+                                if(listId.items[i].id == id){
+                                        $(".detallepeli").html(listId.singleAssamble(listId.items[i]));
+                                        clear();
+                                }
+                        }
                 })
         },
 
         assamble: function (items) {
-                return '<div class="detallepeli" data-id="' + items._id + '">'
-                        + '<img class="" src="' + items.poster + '"></img>'
+                return '<li><div class="Pelicula" data-id=' + items.id + '>'
+                        + '<img class="Poster" src="' + items.poster + '"></img>'
                         + '<p class="nombre">' + items.title + '</p>'
-                        + '<p class="desc"></p>'
-                        + '<p class="p1">1/10</p>'
-                '</div>'
+                        + '<p class="desc">' + items.description + '</p>'
+                        + '<p class="gender">' + items.gender + '</p>'
+                        + '<p class="p1">' + items.score + '</p>'
+                + '</div></li>'
         },
 
-        singleAssamble: function(){
-                return '<ul class="datos">'
-                +'<li class="titulo">'+ items.title +'</li>'
-                +'<li class="imagen">'+ items.poster +'</li>'
-                +'<li class="desc">'+ items.description +'</li>'
-                +'<li class="casting">'+ items.cast +'</li>'
-                +'<li class="genero">'+ items.gender +'</li>'
-                +'<li class="puntaje">'+ items.score +'</li>'
+        singleAssamble: function(items){
+                return '<ul class="Single">'
+                        +'<li> <h3 class="title">'+ items.title +'</p></li>'
+                        +'<li> <img class="PosterSingle" src="'+ items.poster +'"></li>'
+                        +'<li> <p>'+ items.description +'</li>'
+                        +'<li> <p>'+ items.cast +'</li>'
+                        +'<li> <p>'+ items.gender +'</li>'
+                        +'<li> <p> Puntaje: <span class="Puntaje">'+ items.score +'</span></li>'
+                        +'<button class="Volver" onClick=clearSingle()>Volver</button>'
                 +'</ul>'
         },
 
 };
 
+function getPelis(id) {
+        return $.ajax({
+                url: "http://192.168.1.115:3000/peliculas/" + id,
+                method: "GET"
+       })
+}
+
 
 $(function () {
-        getAllFilms();
+        listId.items = peliculones;
+        listId.listAll();
 })
-/*
-        function cleartable(){
-                $("#traer").html(" ");
-             }; 
-*/
+
+function clear(){
+        $(".Contenedor").html("");
+};
+
+function clearSingle(){
+        $(".detallepeli").html("");
+        listId.listAll();
+}
